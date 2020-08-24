749326a4f41365e0cbd64b7571eab564f52f902d
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-24 12:36:46 +0200

    Added login with phone number via firebase auth

---
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/Common/Common.java
@@ -0,0 +1,8 @@
+package com.ganarstudio.orderfoodappjava.Common;
+
+import com.ganarstudio.orderfoodappjava.Model.UserModel;
+
+public class Common {
+    public static final String USER_REFERENCES = "Users";
+    public static UserModel currentUser;
+}
--- app/src/main/java/com/ganarstudio/orderfoodappjava/MainActivity.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/MainActivity.java
@@ -1,14 +1,177 @@
 package com.ganarstudio.orderfoodappjava;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.ganarstudio.orderfoodappjava.Common.Common;
+import com.ganarstudio.orderfoodappjava.Model.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int APP_REQUEST_CODE = 7171;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+    private AlertDialog dialog;
+
+    private DatabaseReference userRef;
+    private List<AuthUI.IdpConfig> providers;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if (listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+
+        userRef = FirebaseDatabase.getInstance().getReference(Common.USER_REFERENCES);
+        firebaseAuth = FirebaseAuth.getInstance();
+        dialog = new SpotsDialog.Builder().setCancelable(false).setContext(this).build();
+        listener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                checkUserFromFirebase(user);
+            } else {
+                phoneLogin();
+            }
+        };
+    }
+
+    private void checkUserFromFirebase(FirebaseUser user) {
+        dialog.show();
+        userRef.child(user.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                            goToHomeActivity(userModel);
+                        } else {
+                            showRegisterDialog(user);
+                        }
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showRegisterDialog(FirebaseUser user) {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder.setTitle("Register");
+        builder.setMessage("Please fill information");
+
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null);
+        EditText edit_name = (EditText) itemView.findViewById(R.id.edit_name);
+        EditText edit_address = (EditText) itemView.findViewById(R.id.edit_address);
+        EditText edit_phone = (EditText) itemView.findViewById(R.id.edit_phone);
+
+        //set data
+        edit_phone.setText(user.getPhoneNumber());
+
+        builder.setNegativeButton("CANCEL", ((dialogInterface, i) -> {
+            dialogInterface.dismiss();
+        }));
+        builder.setPositiveButton("REGISTER", ((dialogInterface, i) -> {
+            if (TextUtils.isEmpty(edit_name.getText().toString())) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (TextUtils.isEmpty(edit_address.getText().toString())) {
+                Toast.makeText(this, "Please enter your address", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            UserModel userModel = new UserModel();
+            userModel.setUid(user.getUid());
+            userModel.setName(edit_name.getText().toString());
+            userModel.setAddress(edit_address.getText().toString());
+            userModel.setPhone(edit_phone.getText().toString());
+
+            userRef.child(user.getUid())
+                    .setValue(userModel)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(this, "Congratulation! Register success", Toast.LENGTH_SHORT).show();
+                            goToHomeActivity(userModel);
+                        }
+                    });
+
+        }));
+
+        builder.setView(itemView);
+
+        androidx.appcompat.app.AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void goToHomeActivity(UserModel userModel) {
+        Common.currentUser = userModel;
+        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        finish();
+    }
+
+    private void phoneLogin() {
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers).build(),
+                APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                Toast.makeText(this, "Failed to sign in!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
