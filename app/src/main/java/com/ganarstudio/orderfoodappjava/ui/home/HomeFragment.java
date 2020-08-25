535d7932aed0317801b699ff1b798556e798bc6a
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-25 21:01:18 +0200

    Set HomeFragment recycler view, can display MyPopularCategoriesAdapter from realtime database in slider photo

--- app/src/main/java/com/ganarstudio/orderfoodappjava/ui/home/HomeFragment.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/ui/home/HomeFragment.java
@@ -10,18 +10,47 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ganarstudio.orderfoodappjava.Adapter.MyPopularCategoriesAdapter;
 import com.ganarstudio.orderfoodappjava.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    Unbinder unbinder;
+
+    @BindView(R.id.recycler_popular)
+    RecyclerView recycler_popular;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, root);
+        init();
+
+        homeViewModel.getPopularList().observe(this, popularCategoryModels ->{
+
+            //create adapter
+            MyPopularCategoriesAdapter adapter = new MyPopularCategoriesAdapter(getContext(), popularCategoryModels);
+            recycler_popular.setAdapter(adapter);
+
+        });
 
         return root;
+    }
+
+    private void init() {
+        //set recycler_view
+        recycler_popular.setHasFixedSize(true);
+        recycler_popular.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
     }
 }
