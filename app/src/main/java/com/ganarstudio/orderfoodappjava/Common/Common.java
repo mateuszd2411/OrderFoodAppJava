bf846175aaf7db68967968a8d2854c63286af10b
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-25 20:51:58 +0200

    Set HomeViewModel(ui), loadPopularList from realtime database

--- app/src/main/java/com/ganarstudio/orderfoodappjava/Common/Common.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/Common/Common.java
@@ -4,5 +4,6 @@ import com.ganarstudio.orderfoodappjava.Model.UserModel;
 
 public class Common {
     public static final String USER_REFERENCES = "Users";
+    public static final String POPULAR_CATEGORY_REF = "MostPopular";
     public static UserModel currentUser;
 }
--- app/src/main/java/com/ganarstudio/orderfoodappjava/ui/home/HomeViewModel.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/ui/home/HomeViewModel.java
@@ -1,19 +1,71 @@
 package com.ganarstudio.orderfoodappjava.ui.home;
 
-import androidx.lifecycle.LiveData;
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+import com.ganarstudio.orderfoodappjava.Callback.IPopularCallbackListener;
+import com.ganarstudio.orderfoodappjava.Common.Common;
+import com.ganarstudio.orderfoodappjava.Model.PopularCategoryModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    private MutableLiveData<String> mText;
+import java.util.ArrayList;
+import java.util.List;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+public class HomeViewModel extends ViewModel implements IPopularCallbackListener {
+
+    private MutableLiveData<List<PopularCategoryModel>> popularList;
+    private MutableLiveData<String> messageError;
+    private IPopularCallbackListener popularCallbackListener;
+
+    public MutableLiveData<List<PopularCategoryModel>> getPopularList() {
+        if (popularList == null) {
+            popularList = new MutableLiveData<>();
+            messageError = new MutableLiveData<>();
+            loadPopularList();
+        }
+        return popularList;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    private void loadPopularList() {
+        List<PopularCategoryModel> tempList = new ArrayList<>();
+        DatabaseReference popularRef = FirebaseDatabase.getInstance().getReference(Common.POPULAR_CATEGORY_REF);
+        popularRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                    PopularCategoryModel model = itemSnapshot.getValue(PopularCategoryModel.class);
+                    tempList.add(model);
+                }
+                popularCallbackListener.onPopularLoadSuccess(tempList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                popularCallbackListener.onPopularLoadFailed(error.getMessage());
+            }
+        });
+    }
+
+    public MutableLiveData<String> getMessageError() {
+        return messageError;
+    }
+
+    public HomeViewModel() {
+        popularCallbackListener = this;
+    }
+
+    @Override
+    public void onPopularLoadSuccess(List<PopularCategoryModel> popularCategoryModels) {
+        popularList.setValue(popularCategoryModels);
+    }
+
+    @Override
+    public void onPopularLoadFailed(String message) {
+        messageError.setValue(message);
     }
 }
