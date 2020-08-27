12b48d6d0f17972b4fc7660d92502b63d8c079d2
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-27 18:49:50 +0200

    Set Adapter MyCategoryAdapter recycler view: onCreateViewHolder, onBindViewHolder, getItemCount

--- app/src/main/java/com/ganarstudio/orderfoodappjava/Adapter/MyCategoryAdapter.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/Adapter/MyCategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.ganarstudio.orderfoodappjava.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.ganarstudio.orderfoodappjava.Model.CategoryModel;
 import com.ganarstudio.orderfoodappjava.R;
 
@@ -23,20 +25,28 @@ public class MyCategoryAdapter extends RecyclerView.Adapter<MyCategoryAdapter.My
     Context context;
     List<CategoryModel> categoryModelList;
 
+    public MyCategoryAdapter(Context context, List<CategoryModel> categoryModelList) {
+        this.context = context;
+        this.categoryModelList = categoryModelList;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new MyViewHolder(LayoutInflater.from(context)
+        .inflate(R.layout.layout_category_item,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        Glide.with(context).load(categoryModelList.get(position).getImage())
+                .into(holder.category_image);
+        holder.category_name.setText(new StringBuilder(categoryModelList.get(position).getName()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categoryModelList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
