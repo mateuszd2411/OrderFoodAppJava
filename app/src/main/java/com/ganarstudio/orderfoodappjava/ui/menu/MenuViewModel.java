46bcaac511d97938c26a869b82d5b6e0f718f95d
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-28 12:58:38 +0200

    Set MenuViewModel, loadCategories() from realtime database

--- app/src/main/java/com/ganarstudio/orderfoodappjava/ui/menu/MenuViewModel.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/ui/menu/MenuViewModel.java
@@ -1,22 +1,72 @@
 package com.ganarstudio.orderfoodappjava.ui.menu;
 
-import androidx.lifecycle.LiveData;
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.ganarstudio.orderfoodappjava.Callback.ICategoryCallbackListener;
+import com.ganarstudio.orderfoodappjava.Common.Common;
 import com.ganarstudio.orderfoodappjava.Model.CategoryModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MenuViewModel extends ViewModel {
+public class MenuViewModel extends ViewModel implements ICategoryCallbackListener {
 
     private MutableLiveData<List<CategoryModel>> categoryListMultable;
-    private MutableLiveData<String> messageError;
+    private MutableLiveData<String> messageError = new MutableLiveData<>();
     private ICategoryCallbackListener categoryCallbackListener;
 
     public MenuViewModel() {
-
+        categoryCallbackListener = this;
     }
 
+    public MutableLiveData<List<CategoryModel>> getCategoryListMultable() {
+        if (categoryListMultable == null) {
+            categoryListMultable = new MutableLiveData<>();
+            messageError = new MutableLiveData<>();
+            loadCategories();
+        }
+        return categoryListMultable;
+    }
+
+    private void loadCategories() {
+        List<CategoryModel> tempList = new ArrayList<>();
+        DatabaseReference categoryRef = FirebaseDatabase.getInstance().getReference(Common.CATEGORY_REF);
+        categoryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot itemSnapshot : snapshot.getChildren()) {
+                    CategoryModel categoryModel = itemSnapshot.getValue(CategoryModel.class);
+                    categoryModel.setMenu_id(itemSnapshot.getKey());
+                    tempList.add(categoryModel);
+                }
+                categoryCallbackListener.onCategoryLoadSuccess(tempList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                categoryCallbackListener.onCategoryLoadFailed(error.getMessage());
+            }
+        });
+    }
+
+    public MutableLiveData<String> getMessageError() {
+        return messageError;
+    }
+
+    @Override
+    public void onCategoryLoadSuccess(List<CategoryModel> categoryModelList) {
+        categoryListMultable.setValue(categoryModelList);
+    }
+
+    @Override
+    public void onCategoryLoadFailed(String message) {
+        messageError.setValue(message);
+    }
 }
