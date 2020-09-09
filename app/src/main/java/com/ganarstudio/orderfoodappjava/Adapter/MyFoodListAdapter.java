418896780184f69335528b42debc536462db1117
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-09 16:17:22 +0200

    Set onCreateViewHolder, onBindViewHolder, getItemCount in MyFoodListAdapter

--- app/src/main/java/com/ganarstudio/orderfoodappjava/Adapter/MyFoodListAdapter.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/Adapter/MyFoodListAdapter.java
@@ -1,5 +1,7 @@
 package com.ganarstudio.orderfoodappjava.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,7 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.ganarstudio.orderfoodappjava.Model.FoodModel;
 import com.ganarstudio.orderfoodappjava.R;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,20 +21,33 @@ import butterknife.Unbinder;
 
 public class MyFoodListAdapter extends RecyclerView.Adapter<MyFoodListAdapter.MyViewHolder> {
 
+    private Context context;
+    private List<FoodModel> foodModelList;
+
+    public MyFoodListAdapter(Context context, List<FoodModel> foodModelList) {
+        this.context = context;
+        this.foodModelList = foodModelList;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new MyViewHolder(LayoutInflater.from(context)
+        .inflate(R.layout.layout_food_item,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        Glide.with(context).load(foodModelList.get(position).getImage()).into(holder.img_food_image);
+        holder.txt_food_price.setText(new StringBuilder("$")
+                .append(foodModelList.get(position).getPrice()));
+        holder.txt_food_name.setText(new StringBuilder("")
+        .append(foodModelList.get(position).getName()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return foodModelList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
