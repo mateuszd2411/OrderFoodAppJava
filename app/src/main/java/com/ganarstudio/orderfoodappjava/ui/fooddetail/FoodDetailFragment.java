f0f358327fbbab6f7997cb2f987f9ae806128706
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-25 20:41:24 +0200

    Init views butter knife, FoodDetailFragment

--- app/src/main/java/com/ganarstudio/orderfoodappjava/ui/fooddetail/FoodDetailFragment.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/ui/fooddetail/FoodDetailFragment.java
@@ -4,17 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.andremion.counterfab.CounterFab;
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.ganarstudio.orderfoodappjava.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.Unbinder;
 
 public class FoodDetailFragment extends Fragment {
 
     private FoodDetailViewModel slideshowViewModel;
+
+    private Unbinder unbinder;
+    @BindView(R.id.img_food)
+    ImageView img_food;
+    @BindView(R.id.btnCart)
+    CounterFab btnCart;
+    @BindView(R.id.btn_rating)
+    FloatingActionButton btn_rating;
+    @BindView(R.id.food_name)
+    TextView food_name;
+    @BindView(R.id.food_description)
+    TextView food_description;
+    @BindView(R.id.txt_food_price)
+    TextView food_price;
+    @BindView(R.id.number_button)
+    ElegantNumberButton numberButton;
+    @BindView(R.id.ratingBar)
+    RatingBar ratingBar;
+    @BindView(R.id.btnShowComment)
+    Button btnShowComment;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
