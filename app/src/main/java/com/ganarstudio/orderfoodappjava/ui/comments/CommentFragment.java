aeafc863c1f46f88b7cf735bf71ef489deed1e43
mateuszd2411 <mateuszd2411@gmail.com>
2020-10-11 17:39:47 +0200

    CommentFragment: init views, dialog and set recycler comment

--- app/src/main/java/com/ganarstudio/orderfoodappjava/ui/comments/CommentFragment.java
+++ app/src/main/java/com/ganarstudio/orderfoodappjava/ui/comments/CommentFragment.java
@@ -1,12 +1,76 @@
 package com.ganarstudio.orderfoodappjava.ui.comments;
 
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.ganarstudio.orderfoodappjava.Callback.ICommentCallbackListener;
 import com.ganarstudio.orderfoodappjava.Model.CommentModel;
+import com.ganarstudio.orderfoodappjava.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import dmax.dialog.SpotsDialog;
+
 public class CommentFragment extends BottomSheetDialogFragment implements ICommentCallbackListener {
+
+    private CommentViewModel commentViewModel;
+
+    private Unbinder unbinder;
+
+    @BindView(R.id.recycler_comment)
+    RecyclerView recycler_comment;
+
+    AlertDialog dialog;
+    ICommentCallbackListener listener;
+
+    public CommentFragment() {
+        listener = this;
+    }
+
+    private static CommentFragment instance;
+
+    public static CommentFragment getInstance() {
+        if (instance == null)
+            instance = new CommentFragment();
+        return instance;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View itemView = LayoutInflater.from(getContext())
+                .inflate(R.layout.bottom_sheet_fragment, container, false);
+        unbinder = ButterKnife.bind(this, itemView);
+        initViews();
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private void initViews() {
+        commentViewModel = ViewModelProviders.of(this).get(CommentViewModel.class);
+        dialog = new SpotsDialog.Builder().setContext(getContext()).setCancelable(false).build();
+
+        recycler_comment.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, true);
+        recycler_comment.setLayoutManager(layoutManager);
+        recycler_comment.addItemDecoration(new DividerItemDecoration(getContext(), layoutManager.getOrientation()));
+    }
+
     @Override
     public void onCommentLoadSuccess(List<CommentModel> commentModels) {
 
